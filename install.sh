ae8e63130008780570fdf8cb1105cc253928ccaf
Yusuf Hadiwinata Sutandar <yusuf.hadiwinata@gmail.com>
2018-10-06 17:11:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,30 @@
   # install Kubernetes
   # 
   kubeadm init --apiserver-advertise-address=172.16.0.22  --pod-network-cidr=192.168.0.0/16
+  
+  # COpy credential to home dir and set permission
+  mkdir -p $HOME/.kube
   cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  chown $(id -u):$(id -g) $HOME/.kube/config
+  
+  # join node to msater
+  kubeadm join 172.16.0.22:6443 --token ly4swk.uyxl37ovhi0m7ktt --discovery-token-ca-cert-hash <token-hash>
+  kubectl get nodes
+  
+   # Deploy CNI using weave
   kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+  
+  # make sure core-dns is up and running
   watch -n 2 kubectl get pods --all-namespaces -o wide
   kubectl get nodes
+  
+  # Install ISTIO
+  cd /root/
+  curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.0.0 sh -
+  export PATH="$PATH:/root/istio-1.0.0/bin"
+  cd /root/istio-1.0.0
+
+  
   591  kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
   592  watch -n 2 kubectl get pods --all-namespaces -o wide
   593  kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/calicoctl.yaml
