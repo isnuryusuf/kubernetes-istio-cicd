e911595dbdbabe10be517d527d58d05c464ab2e4
Yusuf Hadiwinata Sutandar <yusuf.hadiwinata@gmail.com>
2018-10-11 09:22:56 +0700

    Create cicd.sh

---
+++ cicd.sh
@@ -0,0 +1,89 @@
+
+vim Dockerfile
+
+from jenkins/jenkins:2.60.3
+
+# Distributed Builds plugins
+RUN /usr/local/bin/install-plugins.sh ssh-slaves
+
+# install Notifications and Publishing plugins
+RUN /usr/local/bin/install-plugins.sh email-ext
+RUN /usr/local/bin/install-plugins.sh mailer
+RUN /usr/local/bin/install-plugins.sh slack
+
+# Artifacts
+RUN /usr/local/bin/install-plugins.sh htmlpublisher
+
+# UI
+RUN /usr/local/bin/install-plugins.sh greenballs
+RUN /usr/local/bin/install-plugins.sh simple-theme-plugin
+
+# Scaling
+RUN /usr/local/bin/install-plugins.sh kubernetes
+
+# install Maven
+USER root
+RUN apt-get update && apt-get install -y maven
+USER jenkins
+
+
+docker build -t isnuryusuf/jenkins-imange:2.60.3 .
+docker login
+docker push  isnuryusuf/jenkins-imange:2.60.3
+
+
+jenkins-deployment.yaml
+
+apiVersion: extensions/v1beta1
+kind: Deployment
+metadata:
+  name: jenkins
+spec:
+  replicas: 1
+  template:
+    metadata:
+      labels:
+        app: jenkins
+    spec:
+      containers:
+        - name: jenkins
+          image: isnuryusuf/jenkins-image:2.121.2
+          env:
+            - name: JAVA_OPTS
+              value: -Djenkins.install.runSetupWizard=false
+          ports:
+            - name: http-port
+              containerPort: 8080
+            - name: jnlp-port
+              containerPort: 50000
+          volumeMounts:
+            - name: jenkins-home
+              mountPath: /var/jenkins_home
+      volumes:
+        - name: jenkins-home
+          emptyDir: {}
+
+kubectl apply -f jenkins-deployment.yaml
+
+jenkins-service.yaml
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: jenkins
+spec:
+  type: NodePort
+  ports:
+    - port: 8080
+      targetPort: 8080
+  selector:
+    app: jenkins
+
+kubectl create -f jenkins-service.yaml
+
+kubectl get pod ; kubectl get svc
+
+
+
+
+
